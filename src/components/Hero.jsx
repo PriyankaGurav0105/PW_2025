01e460080730a3c5cc18fae0211c6d76922e0f87
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-20 17:30:49 +0530

    Profile updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import profilePic from '../assets/raviKumarProfile.webp'
+import profilePic from '../assets/priyankaProfile.png'
 import {HERO_CONTENT} from '../constants/index.js'
 import {motion} from "framer-motion"
 
